46a926d65831ff473db4746e6f0ec3e6d44a0ef2
Adnan Khan <adnan.khan@cdk.com>
2016-04-03 22:32:06 +0530

    Enabling Spring Security

--- src/main/java/hello/WebServiceConfig.java
+++ src/main/java/hello/WebServiceConfig.java
@@ -18,6 +18,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	@Bean
 	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
 		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+
 		servlet.setApplicationContext(applicationContext);
 		servlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean(servlet, "/ws/*");
